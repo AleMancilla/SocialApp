9c8a9b8f5648346a8489dd0b8a0c5234cd1ba32c
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2024-10-21 02:17:13 -0400

    save last position of widget

--- android/app/src/main/kotlin/com/example/wenia_assignment/FloatingWidgetService.kt
+++ android/app/src/main/kotlin/com/example/wenia_assignment/FloatingWidgetService.kt
@@ -1,7 +1,8 @@
 package com.alecodeando.weniatest
-
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Build
 import android.os.Handler
 import android.os.IBinder
@@ -14,13 +15,14 @@ import android.graphics.PixelFormat
 import android.view.Gravity
 import android.view.MotionEvent
 import android.graphics.Color
-
+import android.graphics.drawable.GradientDrawable
 
 class FloatingWidgetService : Service() {
 
     private lateinit var floatingView: View
     private lateinit var windowManager: WindowManager
     private val handler = Handler()
+    private lateinit var sharedPreferences: SharedPreferences
 
     private var initialX = 0
     private var initialY = 0
@@ -31,15 +33,12 @@ class FloatingWidgetService : Service() {
         try {
             intent?.let {
                 when (it.action) {
-                    "CLOSE_WIDGET" -> closeWidget() // Cerrar el widget flotante
+                    "CLOSE_WIDGET" -> closeWidget()
                     else -> {
                         val usageTime = it.getStringExtra("usage_time")
-                        usageTime?.let { time -> 
-                            // Convertir tiempo a segundos
+                        usageTime?.let { time ->
                             val timeParts = time.split(":").map { part -> part.toIntOrNull() ?: 0 }
                             val usageSeconds = timeParts[0] * 3600 + timeParts[1] * 60 + timeParts[2]
-
-                            // Llamar a updateUsageTime con ambos parámetros
                             updateUsageTime(time, usageSeconds)
                         }
                     }
@@ -56,6 +55,9 @@ class FloatingWidgetService : Service() {
     override fun onCreate() {
         super.onCreate()
 
+        // Inicializar SharedPreferences
+        sharedPreferences = getSharedPreferences("FloatingWidgetPrefs", Context.MODE_PRIVATE)
+
         // Inflar el diseño del widget flotante
         floatingView = LayoutInflater.from(this).inflate(R.layout.floating_widget_layout, null)
 
@@ -63,15 +65,18 @@ class FloatingWidgetService : Service() {
         val params = WindowManager.LayoutParams(
             WindowManager.LayoutParams.WRAP_CONTENT,
             WindowManager.LayoutParams.WRAP_CONTENT,
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY else WindowManager.LayoutParams.TYPE_PHONE, // Usa TYPE_APPLICATION_OVERLAY para Android O y superiores
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY else WindowManager.LayoutParams.TYPE_PHONE,
             WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
             PixelFormat.TRANSLUCENT
         )
 
-        // Establecer la posición del widget flotante
+        // Restaurar la última posición guardada
+        val savedX = sharedPreferences.getInt("widget_position_x", 0)
+        val savedY = sharedPreferences.getInt("widget_position_y", 100)
+
         params.gravity = Gravity.TOP or Gravity.START
-        params.x = 0
-        params.y = 100 // Cambia la posición Y según sea necesario
+        params.x = savedX
+        params.y = savedY
 
         // Agregar la vista al WindowManager
         windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
@@ -91,6 +96,9 @@ class FloatingWidgetService : Service() {
                     params.x = initialX + (event.rawX - initialTouchX).toInt()
                     params.y = initialY + (event.rawY - initialTouchY).toInt()
                     windowManager.updateViewLayout(floatingView, params)
+
+                    // Guardar la posición actual en SharedPreferences
+                    saveWidgetPosition(params.x, params.y)
                     true
                 }
                 else -> false
@@ -98,29 +106,32 @@ class FloatingWidgetService : Service() {
         }
     }
 
-    // Función para actualizar el tiempo de uso y cambiar el color según los segundos
+    private fun saveWidgetPosition(x: Int, y: Int) {
+        with(sharedPreferences.edit()) {
+            putInt("widget_position_x", x)
+            putInt("widget_position_y", y)
+            apply() // Guardar los cambios de forma asíncrona
+        }
+    }
+
     fun updateUsageTime(usageTime: String, usageSeconds: Int) {
         val usageTextView = floatingView.findViewById<TextView>(R.id.usage_time_text_view)
         usageTextView.text = usageTime
 
-        // Cambiar el color de fondo dependiendo del tiempo en segundos
         val backgroundColor = when {
-            usageSeconds < 1800 -> Color.parseColor("#1B5E20") // Menos de 30 minutos
-            usageSeconds in 1800..2700 -> Color.YELLOW // Entre 30 y 45 minutos
-            else -> Color.RED // Más de 45 minutos
+            usageSeconds < 1800 -> Color.parseColor("#1B5E20")
+            usageSeconds in 1800..2700 -> Color.YELLOW
+            else -> Color.RED
         }
-
-        // Establecer el color de fondo del layout
         floatingView.setBackgroundColor(backgroundColor)
     }
-
 
     private fun closeWidget() {
         Log.d("FloatingWidgetService", "Cerrando widget flotante")
         if (::floatingView.isInitialized) {
             windowManager.removeView(floatingView)
         }
-        stopSelf() // Detener el servicio
+        stopSelf()
     }
 
     override fun onDestroy() {
